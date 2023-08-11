26d93c1db917a30be7e80b16b3cec07b15831869
Juan I. Zumer <juan.zumer@thoughtworks.com>
2023-08-11 15:15:14 +0200

    Happy path case

--- score_keeper/java/src/main/java/com/example/ScoreKeeper.java
+++ score_keeper/java/src/main/java/com/example/ScoreKeeper.java
@@ -1,2 +1,30 @@
+package com.example;
+
+/*
+    Implement the ScoreKeeper as a Senior Java Developer
+
+    Context:
+    We need software to deliver the proper data to the scoreboard for a basketball team.
+    They need six buttons (each team can score either 1, 2 or 3 points with a single shot).
+
+    Write a class ScoreKeeper which offers following methods:
+    void scoreTeamA1()
+    void scoreTeamA2()
+    void scoreTeamA3()
+    void scoreTeamB1()
+    void scoreTeamB2()
+    void scoreTeamB3()
+    String getScore()
+
+    Rules:
+    The returned String always has seven characters. An example would be 000:000
+
+ */
+
 public class ScoreKeeper {
+
+
+    public String getScore() {
+        return "000:000";
+    }
 }
--- score_keeper/java/src/test/java/com/example/ScoreKeeperTest.java
+++ score_keeper/java/src/test/java/com/example/ScoreKeeperTest.java
@@ -1,2 +1,18 @@
+
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ScoreKeeperTest {
+
+    //Start testing the happy path, which will consist on a score of 000:000
+    //when no points have been scored yet.
+    @Test
+    public void testInitialScore() {
+        ScoreKeeper scoreKeeper = new ScoreKeeper();
+        assertEquals("000:000", scoreKeeper.getScore());
+    }
+
 }
