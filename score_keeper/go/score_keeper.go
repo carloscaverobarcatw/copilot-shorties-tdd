2544e1a8d74a590a945177c8f97a680f4342bb46
Juan I. Zumer <juan.zumer@thoughtworks.com>
2023-08-18 14:15:03 +0200

    Happy path test with empty score

--- score_keeper/go/score_keeper.go
+++ score_keeper/go/score_keeper.go
@@ -1,1 +1,35 @@
 package _go
+
+/*
+   Implement the ScoreKeeper as a Senior Go Developer
+
+   Context:
+   We need software to deliver the proper data to the scoreboard for a basketball team.
+   They need six buttons (each team can score either 1, 2 or 3 points with a single shot).
+
+   Write a ScoreKeeper struct which offers following methods:
+   void scoreTeamA1()
+   void scoreTeamA2()
+   void scoreTeamA3()
+   void scoreTeamB1()
+   void scoreTeamB2()
+   void scoreTeamB3()
+   String getScore()
+
+   Rules:
+   The returned String always has seven characters. An example would be 000:000
+*/
+
+// create a struct called ScoreKeeper
+type ScoreKeeper struct {
+}
+
+// create a constructor for ScoreKeeper
+func NewScoreKeeper() ScoreKeeper {
+	return ScoreKeeper{}
+}
+
+// create a method called getScore() that returns a string
+func (sk *ScoreKeeper) GetScore() string {
+	return "000:000"
+}
--- score_keeper/go/score_keeper_test.go
+++ score_keeper/go/score_keeper_test.go
@@ -1,1 +1,13 @@
 package _go
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestEmptyScoreForAMatchWithNoPoints(t *testing.T) {
+	scoreKeeper := NewScoreKeeper()
+	expectedScore := "000:000"
+	actualScore := scoreKeeper.GetScore()
+	assert.Equal(t, expectedScore, actualScore)
+}
