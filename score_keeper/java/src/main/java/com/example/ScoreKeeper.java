ff21eb77b7ff31b7daa1645261fa87884fdcac46
Juan I. Zumer <juan.zumer@thoughtworks.com>
2023-09-12 16:16:55 +0200

    Happy path test

--- score_keeper/java/src/main/java/com/example/ScoreKeeper.java
+++ score_keeper/java/src/main/java/com/example/ScoreKeeper.java
@@ -1,4 +1,35 @@
 package com.example;
 
+/*
+    Implement the ScoreKeeper as a Senior Java Developer
+
+    Context:
+    Implement the software to manage the scoreboard for a basketball team.
+    Each team can score either 1, 2 or 3 points with a single shot.
+
+    Write a class ScoreKeeper which offers following methods:
+    void scoreTeamA1()
+    void scoreTeamA2()
+    void scoreTeamA3()
+    void scoreTeamB1()
+    void scoreTeamB2()
+    void scoreTeamB3()
+    String getScore()
+
+    The returned String always has seven characters. An example would be 000:000
+
+    Rules:
+    Implement the tests step by step. Start with the first test case so teams did not score any points.
+    Continue with the next test case and so on.
+
+    Tests follow the naming convention should<ReturnWhichState><WhenSomethingHappens>.
+    Example "shouldReturn000:000WhenTeamsDidNotScorePoints".
+
+ 		*/
+
 public class ScoreKeeper {
+
+    public String getScore() {
+        return "000:000";
+    }
 }
--- score_keeper/java/src/test/java/com/example/ScoreKeeperTest.java
+++ score_keeper/java/src/test/java/com/example/ScoreKeeperTest.java
@@ -1,4 +1,15 @@
 package com.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ScoreKeeperTest {
+
+    @Test
+    public void shouldReturn000000WhenTeamsDidNotScorePoints() {
+        ScoreKeeper scoreKeeper = new ScoreKeeper();
+        String score = scoreKeeper.getScore();
+        assertEquals("000:000", score);
+    }
 }
