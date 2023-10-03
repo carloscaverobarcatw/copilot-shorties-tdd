0a7574b0e63a7d26059b71c56ba624ef549fd96c
Juan I. Zumer <juan.zumer@thoughtworks.com>
2023-10-03 15:48:46 +0200

    Starting point for workshop

--- score_keeper/java/src/main/java/com/example/ScoreKeeper.java
+++ score_keeper/java/src/main/java/com/example/ScoreKeeper.java
@@ -29,7 +29,4 @@ package com.example;
 
 public class ScoreKeeper {
 
-    public String getScore() {
-        return "000:000";
-    }
 }
--- score_keeper/java/src/test/java/com/example/ScoreKeeperTest.java
+++ score_keeper/java/src/test/java/com/example/ScoreKeeperTest.java
@@ -6,10 +6,5 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ScoreKeeperTest {
 
-    @Test
-    public void shouldReturn000000WhenTeamsDidNotScorePoints() {
-        ScoreKeeper scoreKeeper = new ScoreKeeper();
-        String score = scoreKeeper.getScore();
-        assertEquals("000:000", score);
-    }
+
 }
